22f55f99ccfa7925d4ef95bbfbb0e7be78b7d76a
Yiru-SHA <68270680+Yiru-SHA@users.noreply.github.com>
2020-10-21 21:56:09 +0800

    change script.js

--- public/script.js
+++ public/script.js
@@ -3,8 +3,8 @@ window.addEventListener('load',function(){
     
     let button = document.getElementById('button-log');
     button.addEventListener('click',()=>{
-        let Info = document.getElementById('happy-text').value;
-        console.log(Info);
+        let HappyInfo = document.getElementById('happy-text').value;
+        console.log(HappyInfo);
     })
 
    // make a fetch request - send to server
@@ -28,50 +28,6 @@ window.addEventListener('load',function(){
         .then(data=>{
             console.log(data)
         })
-
-        // window.addEventListener('load',()=> {
-        //     document.getElementById('button-coffee').addEventListener('click', ()=> {
-        //         let noCups = document.getElementById('number-coffee').value;
-        //         console.log(noCups);
-        
-        //         //creating the object 
-        //         let obj = {"number" : noCups};
-        //         //console.log(obj);
-        
-        //         //stringify the object
-        //         let jsonData = JSON.stringify(obj);
-        
-        //         //fetch to route noCups
-        //         fetch('/noCups', {
-        //             method: 'POST',
-        //             headers: {
-        //                 "Content-type": "application/json"
-        //             },
-        //             body: jsonData
-        //         })
-        //         .then(response => response.json())
-        //         .then(data => {console.log(data)});
-        
-        //         //1. make a fetch request of type POST so that we can send the (noCups) info to the server
-        //     })
-        
-        //     document.getElementById('get-tracker').addEventListener('click', ()=> {
-        //         //get info on ALL the coffees we've had so far
-        //         fetch('/getCups')
-        //         .then(resp=> resp.json())
-        //         .then(data => {
-        //             document.getElementById('coffee-info').innerHTML = '';
-        //             console.log(data.data);
-        //             for(let i=0;i<data.data.length;i++) {
-        //                 let string = data.data[i].date + " : " + data.data[i].coffee;
-        //                 let elt = document.createElement('p');
-        //                 elt.innerHTML = string;
-        //                 document.getElementById('coffee-info').appendChild(elt);
-        //             }
-        //         })
-        //     })
-        // })
-
 
 //     document.getElementById('get-tracker').addEventListener('click',()=>{
 //         //统计所有的咖啡总数
